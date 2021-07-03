2156f5c683ed1173be2094b23a29288add9e315d
Christian <tiradoc02@gmail.com>
2021-07-03 16:11:40 -0400

    Core concepts (store, action, reducer, dispatch)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import {createStore} from "redux";
+
+// Store - Global state
+
+// Action - Define what to do
+const petIncrement = () => {
+   return {
+     type: "PET_INCREASED"
+   }
+}
+
+const petDecrement = () => {
+   return {
+     type: "PET_DECREASED"
+   }
+}
+
+// Reducer - Describe how an action change the state into another. Will check which
+const petCounter = (state=0, action) => {
+  switch(action.type){
+    case "PET_INCREASED":
+      return state + 1;
+      break;
+    case "PET_DECREASED":
+      return state - 1;
+      break;
+      default:
+        return state;
+  }
+}
+
+const store = createStore(petCounter);
+
+// action is called and store will be modified
+
+store.subscribe(() => console.log(store.getState()));
+
+// Dispatch - Perform the action to reducer
+store.dispatch(petIncrement());
+store.dispatch(petIncrement());
+store.dispatch(petDecrement());
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Redux Crash Course 2021</h1>
     </div>
   );
 }
