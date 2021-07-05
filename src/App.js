ffbfd38b48e8667072e8304e087c0082a8e36ec8
Christian <tiradoc02@gmail.com>
2021-07-05 09:23:59 -0400

    Accessing state and dispatching actions.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
-import './App.css';
-import {createStore} from "redux";
-import reducers from './reducers';
-
-// Store - Global state
-
-// Action - Define what to do
-const petIncrement = () => {
-   return {
-     type: "PET_INCREASED"
-   }
-}
-
-const petDecrement = () => {
-   return {
-     type: "PET_DECREASED"
-   }
-}
-
-
-
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-// action is called and store will be modified
-
-store.subscribe(() => console.log(store.getState()));
-
-// Dispatch - Perform the action to reducer
-
-
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import {petIncrement, petDecrement} from "./actions";
 function App() {
+  const petCounter = useSelector((state) => state.petCounter);
+  const petFavorite = useSelector((state) => state.petFavorite);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-     <h1>Redux Crash Course 2021</h1>
+      <h1>Redux Crash Course 2021</h1>
+      <button onClick={() => dispatch(petIncrement())}>Add Pet</button>
+      <button onClick={() => dispatch(petDecrement())}>Remove Pet</button>
+      <h2>Pet counter: {petCounter}</h2>
+      <h2>Number of Favorite Pets: {petFavorite}</h2>
     </div>
   );
 }
