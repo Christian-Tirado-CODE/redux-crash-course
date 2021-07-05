7fa26c584497aca6e5e63d08716f5bd472670faa
Christian <tiradoc02@gmail.com>
2021-07-05 09:23:24 -0400

    Setting actions in a separate folder.

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+// Action - Define what to do
+export const petIncrement = () => {
+    return {
+      type: "PET_INCREASED"
+    }
+ }
+ 
+ export const petDecrement = () => {
+    return {
+      type: "PET_DECREASED"
+    }
+ }
